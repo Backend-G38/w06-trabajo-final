c8f831118449db11a916a59e1070daf5c601ffad
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-08-20 00:40:23 -0300

    advances

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,7 @@
 const catchError = require('../utils/catchError');
 const User = require('../models/User');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const getAll = catchError(async (req, res) => {
   const results = await User.findAll();
@@ -11,7 +13,6 @@ const create = catchError(async (req, res) => {
   return res.status(201).json(result);
 });
 
-
 const remove = catchError(async (req, res) => {
   const { id } = req.params;
   const result = await User.destroy({ where: { id } });
@@ -21,6 +22,12 @@ const remove = catchError(async (req, res) => {
 
 const update = catchError(async (req, res) => {
   const { id } = req.params;
+  // impedir que se actualice el mail, phone  y el password
+
+  delete req.body.password
+  delete req.body.email
+  delete req.body.phone
+
   const result = await User.update(
     req.body,
     { where: { id }, returning: true }
@@ -29,9 +36,27 @@ const update = catchError(async (req, res) => {
   return res.json(result[1][0]);
 });
 
+const login = catchError(async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ where: { email } })
+  if (!user) return res.status(401).json({ message: "User not foud" })
+
+  const isValid = await bcrypt.compare(password, user.password)
+  if (!isValid) return res.sendStatus(401)
+
+  const token = jwt.sign(
+    { user },
+    process.env.TOKEN_SECRET,
+    { expiresIn: '1d' }
+  )
+
+  return res.json({ user, token })
+})
+
 module.exports = {
   getAll,
   create,
   remove,
-  update
+  update,
+  login
 }
