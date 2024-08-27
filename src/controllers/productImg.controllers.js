f44ea2af4ea5cc0cf5d81c221793805b3c21926a
Gabriel <martinezgabrielalejandro4@gmail.com>
2024-08-27 01:00:56 -0300

    advances

--- src/controllers/productImg.controllers.js
+++ src/controllers/productImg.controllers.js
@@ -1,5 +1,7 @@
 const catchError = require('../utils/catchError');
 const ProductImg = require('../models/ProductImg');
+const path = require('path')
+const fs = require('fs')
 
 const getAll = catchError(async (req, res) => {
   const results = await ProductImg.findAll();
@@ -21,8 +23,11 @@ const create = catchError(async (req, res) => {
 
 const remove = catchError(async (req, res) => {
   const { id } = req.params;
-  const result = await ProductImg.destroy({ where: { id } });
-  if (!result) return res.sendStatus(404);
+  const image = await ProductImg.findByPk(id)
+  if (!image) return res.sendStatus(404)
+  const imagePath = path.join(__dirname, '..', 'public', 'uploads', `${image.filename}`)
+  fs.unlinkSync(imagePath)
+  await image.destroy();
   return res.sendStatus(204);
 });
 
